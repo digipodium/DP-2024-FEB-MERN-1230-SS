5d4c25b0e04e520412852db7cb59bd20091c42ce
m-triple-m <triplem656@gmail.com>
2024-02-24 16:01:43 +0530

    javascript

---
+++ Section 1/index.js
@@ -0,0 +1,21 @@
+console.log('hello javasrcipt');
+
+console.log('good');
+
+a = 20;
+
+console.log(a);
+
+myname = 'Mubassir';
+
+console.log(myname);
+
+console.log( typeof a );
+
+console.log( 1 + '1' );
+console.log( 5 + 1 + '1' );
+
+console.log( false + 'nice' );
+
+console.log( 1 == '1' );
+console.log( 1 === '1' );
---
+++ Section 1/keyword.js
@@ -0,0 +1,17 @@
+num = 56;
+
+if(num%2 === 0){
+    console.log("The number is even");
+
+    abc = 'something';
+    let x = 'nice';
+    const pi = 3.1416;
+
+    pi = 6.32;
+
+}else{
+    console.log("The number is odd");
+}
+
+console.log(abc);
+console.log(x);
