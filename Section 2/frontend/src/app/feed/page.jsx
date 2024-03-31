a26677be650ed35afec796df3564666cee9eea9c
m-triple-m <triplem656@gmail.com>
2024-03-31 14:54:03 +0530

    update post ui

--- Section 2/frontend/src/app/feed/page.jsx
+++ Section 2/frontend/src/app/feed/page.jsx
@@ -1,6 +1,8 @@
 'use client';
+import { IconHeart, IconMessageCircle2, IconShare3, IconTrash } from '@tabler/icons-react';
 import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
+import ReactTimeAgo from 'react-time-ago';
 
 const Feed = () => {
 
@@ -26,20 +28,20 @@ const Feed = () => {
     }, []);
 
     const deletePost = (id) => {
-        fetch('http://localhost:5000/post/delete/'+id, {
+        fetch('http://localhost:5000/post/delete/' + id, {
             method: 'DELETE'
         })
-        .then((res) => {
-            if(res.status === 200){
-                toast.success('Post deleted successfully');
-                readPostData();
-            }else{
+            .then((res) => {
+                if (res.status === 200) {
+                    toast.success('Post deleted successfully');
+                    readPostData();
+                } else {
+                    toast.error('Error in deleting post');
+                }
+            }).catch((err) => {
+                console.log(err);
                 toast.error('Error in deleting post');
-            }
-        }).catch((err) => {
-            console.log(err);
-            toast.error('Error in deleting post');
-        });
+            });
     }
 
 
@@ -52,15 +54,44 @@ const Feed = () => {
                 {
                     postList.map((post) => {
                         return <div key={post._id} className='card mb-4'>
-                            <div className='card-header'>
-                                <h5>Posted By : {post.username}</h5>
-                                <button className='btn btn-danger' onClick={() => { deletePost(post._id) }}>Delete</button>
+                            <div className='card-header d-flex justify-content-between align-items-start'>
+                                <div>
+                                    <h5>{post.username}</h5>
+                                    <p>
+                                        <ReactTimeAgo date={new Date(post.postedOn)} />
+                                    </p>
+                                </div>
+                                <button className='btn btn-danger' onClick={() => { deletePost(post._id) }}>
+                                    <IconTrash size={25} />
+                                </button>
                             </div>
                             <img className='card-img-fluid' src={post.image} alt="" />
                             <div className='card-body'>
                                 <h5>{post.title}</h5>
-                                <p>{new Date(post.postedOn).toLocaleDateString()}</p>
-                                <button className='btn btn-primary mt-3'> {post.likes} Like</button>
+                                <div className='row border-top border-bottom'>
+                                    <div className="col-4">
+                                        <button className='btn btn-white w-100'>
+                                            <IconHeart size={25} color='#f00' /> 
+                                        </button>
+                                    </div>
+                                    <div className="col-4">
+                                    <button className='btn btn-white w-100'>
+                                            <IconMessageCircle2 size={25} color='#00f' /> 
+                                        </button>
+                                    </div>
+                                    <div className="col-4">
+                                    <button className='btn btn-white w-100'>
+                                            <IconShare3 size={25} /> 
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <p className='mt-3'>
+                                    {post.likes} Likes , 
+                                    0 Comment , 
+                                    {post.shares} Shares
+                                </p>
+                                
                             </div>
                         </div>
                     })
--- Section 2/frontend/src/app/template.jsx
+++ Section 2/frontend/src/app/template.jsx
@@ -2,11 +2,15 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '@/components/navbar';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+
 
 const Template = ({ children }) => {
 
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.min.js');
+    TimeAgo.addDefaultLocale(en);
   }, []);
 
   return (
